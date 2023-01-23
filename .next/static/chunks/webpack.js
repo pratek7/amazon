404e471e06af3662e7f2fe57da195b9cd4153c67
pratek7 <pratikjungkarki123@gmail.com>
2023-01-23 13:30:35 +0545

    comited

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f80452c016c36772"; }
+/******/ 		__webpack_require__.h = function() { return "fed135d9ff0ed817"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
+import Providers from "next-auth/providers";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
+    Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    })
+    }),
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
